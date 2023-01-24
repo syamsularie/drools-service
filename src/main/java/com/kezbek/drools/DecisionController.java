9ebdb51b0f8eb08540f24e7ec3d1203af3cc3796
Syamsul Bachri <syamsul_bachri@telkomsel.co.id>
2023-01-24 13:14:27 +0700

    add docker file

---
+++ src/main/java/com/kezbek/drools/DecisionController.java
@@ -0,0 +1,24 @@
+package com.kezbek.drools;
+
+import com.kezbek.drools.domain.OrderRequest;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DecisionController {
+    private final KieContainer kieContainer;
+    public DecisionController(KieContainer kieContainer) {
+        this.kieContainer = kieContainer;
+    }
+    @PostMapping("/cashback")
+    private OrderRequest getDiscountPercent(@RequestBody OrderRequest orderRequest) {
+        KieSession kieSession = kieContainer.newKieSession();
+        kieSession.insert(orderRequest);
+        kieSession.fireAllRules();
+        kieSession.dispose();
+        return orderRequest;
+    }
+}
---
+++ src/test/java/com/kezbek/drools/DroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kezbek.drools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
